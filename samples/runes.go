2c2ef34be8253fd65ae6f81857158963ff124621
Nikola Zifra <nikola@udrafter.com>
2022-09-11 11:31:57 +0200

    Switched to Builder

--- samples/runes.go
+++ samples/runes.go
@@ -19,7 +19,11 @@ func (ni *NumericInput) Add(r rune) {
 	if !unicode.IsDigit(r) {
 		return
 	}
-	ni.input = strings.Join([]string{ni.input, string(r)}, "")
+	//ni.input = strings.Join([]string{ni.input, string(r)}, "")
+	var b strings.Builder
+	b.WriteString(ni.input)
+	b.WriteString(string(r))
+	ni.input = b.String()
 	fmt.Println("Input changed to ", ni.input)
 }
 
